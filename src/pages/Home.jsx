8a182bf48f809a7d246d024d902aee402f4df109
Donald Dominic <gotodonald@gmail.com>
2023-07-25 03:08:18 +0100

    Feat: Add search box component - Implement a search box component to get weather information - Add styles class to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,13 @@ import bgImage from '../assets/thunderstorm.jpg';
 
 export default function Home() {
     return (
-        <div style={{backgroundImage: `url(${bgImage})` }}>
-            <h1>Real Time Weather App</h1>
-        </div>
+      <div
+        className="bg-cover bg-center bg-no-repeat h-screen p-4"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
+        <h1 className="text-center antialiased text-3xl md:text-4xl lg:text-5xl text-white font-bold pt-8">
+          Real Time Weather App
+        </h1>
+      </div>
     );
 }
