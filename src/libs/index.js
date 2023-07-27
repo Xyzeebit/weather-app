474c8e9af265527fdbc9afef41b38520c3f2f479
Donald Dominic <gotodonald@gmail.com>
2023-07-27 20:56:01 +0100

    Implement a weather api parse functionality
    Create function to get weather report from multiple days
    Add function to get query location from returned data

--- src/libs/index.js
+++ src/libs/index.js
@@ -11,11 +11,49 @@ export function getLocation() {
 }
 
 /**
- * getDays function parses the weather json data and return the days containing 
- * weather results
- * @returns {Array}
+ * getWeatherReport function takes an object, parses it for a weather
+ * report and recursively calls itself for every hour object found in each days report
+ * @param {object} wea - weather JSON object parameter
+ * @returns {object} it returns an object containing the weather report
  */
-export function getDays() {
-    const days = weather.days;
+export function getWeatherReport(wea) {
+    const w = {};
+
+    w["datetime"] = wea.datetime;
+    w["temperature"] = wea.temp;
+    w["dew"] = wea.dew;
+    w["humidity"] = wea.humidity;
+    w["windspeed"] = wea.windspeed;
+    w["pressure"] = wea.pressure;
+    w["cloudcover"] = wea.cloudcover;
+    w["visibility"] = wea.visibility;
+    w["uvindex"] = wea.uvindex;
+    w["sunrise"] = wea.sunrise;
+    w["sunset"] = wea.sunset;
+    w["conditions"] = wea.conditions;
+    w["description"] = wea.description;
+    w["icon"] = wea.icon;
+
+    if (wea.hours && wea.hours.length > 0) {
+        w["hours"] = [];
+        for (let wh of wea.hours) {
+            const report = getWeatherReport(wh);
+            w.hours.push(report);
+        }
+    }
+
+    return w;
+}
+
+/**
+ * getMainWeatherFromDays function parses the weather object for a weather report and
+ * returns an array of weather report objects
+ */
+export function getMainWeatherFromDays() {
+    const days = [];
+    for (let w of weather.days) {
+        const report = getWeatherReport(w);
+        days.push(report);
+    }
     return days;
 }
