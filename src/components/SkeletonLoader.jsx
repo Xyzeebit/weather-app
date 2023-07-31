7d508878ab983d1947751a96c1549f04f5336878
Donald Dominic <gotodonald@gmail.com>
2023-07-31 17:40:30 +0100

    Implement responsive skeleton loader

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -1,8 +1,8 @@
 const SkeletonLoader = () => (
     <div className="h-screen overflow-hidden bg-white px-4 md:px-24">
-        <div className="flex justify-between items-center gap-8 pt-10">
-            <div className="skeleton-loader w-1/3 h-80 " />
-            <div className="bg-white w-2/3 h-80">
+        <div className="md:flex justify-between items-center gap-8 pt-10">
+            <div className="skeleton-loader mb-10 md:mb-0 w-full md:w-1/3 h-80 " />
+            <div className="bg-white md:w-2/3 h-80">
                 <div className="skeleton-loader h-24 mb-10" />
                 <div className="skeleton-loader h-full" />
             </div>
